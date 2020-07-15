0799ae177425afe5a08424d7b9ec1443359afd1c
Margarita Morozova <ritamorozova@yahoo.com>
2020-07-15 16:52:06 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,11 +1,14 @@
 def nyc_pigeon_organizer(data)
   final_result = data.each_with_object({}) do |(key, value),  final_array| 
-    value.each do |inner_key, names| #
-      names.each do |name|
-        if !final_array[name]
+    
+    value.each do |inner_key, names| # inner_key = [:purple], names = [array of names]
+    
+       names.each do |name|
+        if !final_array[name] #if there is no Theo, create a new hash
           final_array[name] = {}
         end
-        if !final_array[name][key]
+        
+        if !final_array[name][key] #if there is no Theo[:color]
           final_array[name][key] = []
         end 
         final_array[name][key].push(inner_key.to_s)
